234aa50aac3efb5f1f00e21d50dce3b3e8fdee69
otohusan <tamioguitar@gmail.com>
2025-04-08 21:16:34 +0900

    testが間違うことを確認

--- test/rgb_test.rb
+++ test/rgb_test.rb
@@ -4,5 +4,6 @@ require_relative '../lib/rgb'
 class RGBTest < Minitest::Test
   def test_to_hex
     assert_equal '#00000', to_hex(0, 0, 0)
+    assert_equal '#fffff', to_hex(255, 255, 255)
   end
 end
