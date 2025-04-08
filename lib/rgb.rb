5980be8ab3efae2125753da576a497f85b98e780
otohusan <tamioguitar@gmail.com>
2025-04-08 18:56:12 +0900

    配列を文字にする使い分け

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -7,4 +7,13 @@ a = [1, 2, 3, 4, 5, 6, 7, 8]
 a.delete_if do |x|
   x > 5
 end
-print a
+puts a
+
+# 配列を文字列にする方法の使い分け（join, sum）
+chars = %w[a b c]
+# 文字列に変換
+puts chars.join
+# 文字列に変換（区切り文字を指定）
+puts chars.join('-')
+# 文字列に変換(先頭に文字追加や文字を加工する場合)
+puts chars.sum('<') { |x| x.upcase }
