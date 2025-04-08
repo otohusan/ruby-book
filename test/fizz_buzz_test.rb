64b1a55b0becea1fde9c2a32204c52124bf5fdec
otohusan <tamioguitar@gmail.com>
2025-04-08 18:09:33 +0900

    ファイルの拡張子が間違っていた

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -1,21 +1,14 @@
 require 'minitest/autorun'
-
-def fizz_buzz(n)
-  if n % 15 == 0
-    'FizzBuzz'
-  elsif n % 3 == 0
-    'Fizz'
-  elsif n % 5 == 0
-    'Buzz'
-  else
-    n.to_s
-  end
-end
+require_relative '../lib/fizz_buzz'
 
 class FizzBuzzTest < Minitest::Test
   def test_fizz_buzz
     assert_equal '1', fizz_buzz(1)
     assert_equal '2', fizz_buzz(2)
     assert_equal 'Fizz', fizz_buzz(3)
+    assert_equal '4', fizz_buzz(4)
+    assert_equal '5', fizz_buzz(5)
+    assert_equal '6', fizz_buzz(6)
+    assert_equal '2', fizz_buzz(15)
   end
 end
