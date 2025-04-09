0f6c83e5d10d441a2aa0dc9f2dee693f1f5c0751
otohusan <tamioguitar@gmail.com>
2025-04-09 17:33:21 +0900

    失敗するテストデータを追加

--- test/convert_hash_syntax_test.rb
+++ test/convert_hash_syntax_test.rb
@@ -3,6 +3,14 @@ require_relative '../lib/convert_hash_syntax'
 
 class ConvertHashSyntaxTest < Minitest::Test
   def test_convert_hash_syntax
+    old_syntax = <<~TEXT
+      { :name => 'Alice', :age => 30, :city => 'Tokyo' }
+    TEXT
+    expected = <<~TEXT
+      { name: 'Alice', age: 30, city: 'Tokyo' }
+    TEXT
+
     assert_equal '{}', convert_hash_syntax('{}')
+    assert_equal expected, convert_hash_syntax(old_syntax)
   end
 end
