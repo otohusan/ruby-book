afe108237bf59a5aa1b8d54b494be119ba2d8cc0
otohusan <tamioguitar@gmail.com>
2025-04-09 16:34:51 +0900

    正規表現での置換

--- lib/convert_hash_syntax.rb
+++ lib/convert_hash_syntax.rb
@@ -1,3 +1,4 @@
+# 正規表現の使用例
 text = <<TEXT
   I love RUBY
   Python is great language
@@ -5,3 +6,12 @@ text = <<TEXT
 TEXT
 
 puts text.scan(/[A-Z][A-Za-z]+/)
+
+# 正規表現を使用した置換の例
+text = <<TEXT
+  私の郵便番号は1234567です。
+  僕の郵便番号は9876543です。
+  兵庫県は1234567です。
+TEXT
+
+puts text.gsub(/(\d{3})(\d{4})/, '\1-\2')
