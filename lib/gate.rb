118d2acdb78fe54a294a10bf8706878df5d4b16a
otohusan <tamioguitar@gmail.com>
2025-04-10 17:10:29 +0900

    クラスの作成例

--- lib/gate.rb
+++ lib/gate.rb
@@ -1,2 +1,19 @@
+# クラスの作成例
+class User
+  # 外部からアクセス可能な属性を定義するためのアクセサメソッドを使用
+  attr_accessor :name, :age
+
+  # オブジェクト作成時に呼び出す
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  # インスタンスメソッドの定義
+  def hello
+    puts "Hello, #{@name}!"
+  end
+end
+
 class Gate
 end
