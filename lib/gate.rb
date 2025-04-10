1d9b6b24f5a62ab0ef970f46b42b3d468bd1c785
otohusan <tamioguitar@gmail.com>
2025-04-10 17:14:48 +0900

    クラスメソッドの定義

--- lib/gate.rb
+++ lib/gate.rb
@@ -1,10 +1,12 @@
 # クラスの作成例
 class User
   # 外部からアクセス可能な属性を定義するためのアクセサメソッドを使用
+  # 読み取り、書き込みそれぞれの専用アクセサメソッドもある
   attr_accessor :name, :age
 
-  # オブジェクト作成時に呼び出す
+  # インスタンス作成時に呼び出す
   def initialize(name, age)
+    # インスタンス変数を定義
     @name = name
     @age = age
   end
@@ -13,6 +15,15 @@ class User
   def hello
     puts "Hello, #{@name}!"
   end
+
+  # クラスメソッドの定義
+  def self.hello
+    p 'Hello, class method!'
+  end
+  # 以下のような書き方もある
+  # class << self
+  #   def ~
+  # end
 end
 
 class Gate
