fdeca864d9374f7d2796161a736fac54dc7fb81e
otohusan <tamioguitar@gmail.com>
2025-04-11 17:15:50 +0900

    moduleをincludeして使う

--- lib/rainbowable.rb
+++ lib/rainbowable.rb
@@ -4,3 +4,11 @@ module Greetable
     puts 'Hello from Greetable module!'
   end
 end
+
+class Person
+  # modeuleをincludeすることで、モジュールのメソッドをクラスに追加できる
+  include Greetable
+end
+
+person = Person.new
+person.hello #=> Hello from Greetable module!
