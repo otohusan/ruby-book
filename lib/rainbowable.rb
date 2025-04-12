752a8ee7a0269e9d2e00e2d52f31e2ed33ee7ce0
otohusan <tamioguitar@gmail.com>
2025-04-12 10:35:51 +0900

    moduleをprivateに

--- lib/rainbowable.rb
+++ lib/rainbowable.rb
@@ -3,12 +3,22 @@ module Greetable
   def hello
     puts 'Hello from Greetable module!'
   end
+
+  # privateメソッドを定義する
+  # includeされたクラスでもprivateだと扱われる
+  private
+
+  def private_method
+    puts 'This is a private method from Greetable module.'
+  end
 end
 
 class Person
-  # modeuleをincludeすることで、モジュールのメソッドをクラスに追加できる
+  # moduleをincludeすることで、モジュールのメソッドをクラスに追加できる
+  # (ミックスイン)
   include Greetable
 end
 
 person = Person.new
 person.hello #=> Hello from Greetable module!
+# person.private_method => private_methodは呼び出せない
