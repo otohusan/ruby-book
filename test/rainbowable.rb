56183249d52d1f2713f874150e77b20fbfd11505
otohusan <tamioguitar@gmail.com>
2025-04-12 11:33:35 +0900

    setupの追加

--- test/rainbowable.rb
+++ test/rainbowable.rb
@@ -2,6 +2,11 @@ require 'Minitest/autorun'
 require_relative '../lib/rainbowable'
 
 class RainbowableTest < Minitest::Test
+  def setup
+    String.include Rainbowable
+    Array.include Rainbowable
+  end
+
   def test_rainbow
     assert Rainbowable
   end
