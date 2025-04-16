6e4e03bce4947371bbc59e55d539f22d930d3782
otohusan <tamioguitar@gmail.com>
2025-04-16 14:41:15 +0900

    例外クラスへの補足

--- lib/exception.rb
+++ lib/exception.rb
@@ -2,8 +2,14 @@
 begin
   1 + '1'
   # エラーの情報を格納する
+  # 例外オブジェクトのクラスが一致したものだけをキャッチする
 rescue StandardError => e
   p '例外が発生しました'
   p e.class
   p e.message
+
+  # 上にあるrescueでキャッチしてしまうエラーだから意味がない
+  # 例外クラスの継承関係も大事
+  # rescue NameError => e
+  #   p 'NameErrorが発生しました'
 end
