5872842953817e5dd24c18c9a232660ba428f59d
otohusan <tamioguitar@gmail.com>
2025-04-16 14:48:07 +0900

    retry例

--- lib/exception.rb
+++ lib/exception.rb
@@ -1,15 +1,27 @@
+retry_count = 0
 # 例外処理
 begin
   1 + '1'
   # エラーの情報を格納する
   # 例外オブジェクトのクラスが一致したものだけをキャッチする
-rescue StandardError => e
+rescue NameError => e
   p '例外が発生しました'
   p e.class
   p e.message
 
   # 上にあるrescueでキャッチしてしまうエラーだから意味がない
   # 例外クラスの継承関係も大事
-  # rescue NameError => e
+  # rescue NoMethodError => e
   #   p 'NameErrorが発生しました'
+
+  # begin節をやり直したい時はretryを使う
+  # 無限ループしないように、カウンタ変数を用意する
+rescue StandardError => e
+  retry_count += 1
+  if retry_count < 3
+    puts "リトライします。#{retry_count}回目"
+    retry
+  else
+    puts '3回リトライしても失敗しました'
+  end
 end
