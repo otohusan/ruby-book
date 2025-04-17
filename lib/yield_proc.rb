ef57384fe5350443a2b2f7fe054b457c5053c4fa
otohusan <tamioguitar@gmail.com>
2025-04-17 22:22:39 +0900

    ブロックを引数として

--- lib/yield_proc.rb
+++ lib/yield_proc.rb
@@ -15,3 +15,16 @@ end
 greet do
   puts 'こんにちは'
 end
+
+# ブロックを引数として受け取る
+def greet2(&block)
+  puts 'おはよう'
+
+  puts block.call('こんにちは')
+
+  puts 'こんばんは'
+end
+
+greet2 do |msg|
+  puts msg * 2
+end
