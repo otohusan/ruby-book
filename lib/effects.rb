e4799ae311dc9a4b0bb6d23c97c0657fc61bab64
otohusan <tamioguitar@gmail.com>
2025-04-18 10:07:42 +0900

    echoの実装

--- lib/effects.rb
+++ lib/effects.rb
@@ -5,4 +5,10 @@ module Effects
       word.split(' ').map(&:reverse).join(' ')
     end
   end
+
+  def self.echo(rate = 1)
+    lambda do |word|
+      word.each_char.map { |char| char == ' ' ? ' ' : char * rate }.join
+    end
+  end
 end
--- test/effects_test.rb
+++ test/effects_test.rb
@@ -5,4 +5,9 @@ class EffectsTest < Minitest::Test
   def test_reverse
     assert_equal 'olleH dlrow', Effects.reverse.call('Hello world')
   end
+
+  def test_echo
+    assert_equal 'HHeelllloo wwoorrlldd', Effects.echo(2).call('Hello world')
+    assert_equal 'HHHeeellllllooo wwwooorrrlllddd', Effects.echo(3).call('Hello world')
+  end
 end
