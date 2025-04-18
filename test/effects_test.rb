0db9cbe9a8f78125ede20f97e037ebeb58510643
otohusan <tamioguitar@gmail.com>
2025-04-18 09:54:19 +0900

    reverseのテスト

--- test/effects_test.rb
+++ test/effects_test.rb
@@ -3,6 +3,6 @@ require_relative '../lib/effects'
 
 class EffectsTest < Minitest::Test
   def test_reverse
-    assert Effects
+    assert_equal 'olleH dlrow', Effects.reverse.call('Hello world')
   end
 end
