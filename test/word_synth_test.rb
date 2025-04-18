10696c00bc1d358ebe291fff652517054e74a915
otohusan <tamioguitar@gmail.com>
2025-04-18 12:39:37 +0900

    複数のeffectをつけたテストを追加

--- test/word_synth_test.rb
+++ test/word_synth_test.rb
@@ -15,4 +15,11 @@ class WordSynthTest < Minitest::Test
     @synth.add_effect(Effects.reverse)
     assert_equal 'olleH dlrow', @synth.play('Hello world')
   end
+
+  def test_play_with_many_effects
+    @synth.add_effect(Effects.reverse)
+    @synth.add_effect(Effects.echo(2))
+    @synth.add_effect(Effects.loud(4))
+    assert_equal 'OOLLLLEEHH!!!! DDLLRROOWW!!!!', @synth.play('Hello world')
+  end
 end
