f3883e5c2527fba9676513406b22498393863c40
otohusan <tamioguitar@gmail.com>
2025-04-22 16:08:21 +0900

    コメントを追加

--- lib/log_formatter.rb
+++ lib/log_formatter.rb
@@ -5,8 +5,11 @@ require 'uri'
 # logを整形するモジュール
 module LogFormatter
   def self.format_log
+    # 文字列をURI::HTTPオブジェクトに変換して、HTTPリクエストを作成
     uri = URI.parse('https://samples.jnito.com/access-log.json')
     json = Net::HTTP.get(uri)
+
+    # symbolize_names: trueは keyをシンボルにしてる
     log = JSON.parse(json, symbolize_names: true)
     pp log
   end
