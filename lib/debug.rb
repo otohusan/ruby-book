8b70bad76894d6a6b3cca3066005722b2c662a99
otohusan <tamioguitar@gmail.com>
2025-04-23 16:53:45 +0900

    一部をcommit

---
+++ lib/debug.rb
@@ -0,0 +1,6 @@
+# よく発生する例外クラス
+# NameError -> 定義されていないクラスやメソッドを呼び出そうとした
+# NoMethodError -> 定義されていないメソッドを呼び出そうとした
+# ArgumentError -> メソッドに渡す引数の数が違う
+# TypeError -> 型が違う
+# ZeroDivisionError -> 0で割ろうとした
